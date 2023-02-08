1026cd2472afb69f0189eb0ca48176dc1b1ec77c
danros <dan.ros@blecon.net>
2023-02-08 00:08:32 +0000

    Prepare echo example

--- process_request/app.py
+++ process_request/app.py
@@ -1,23 +1,7 @@
-import os
 import json
-import requests
-
 
 def lambda_handler(event, context):
-    # forward request
+    # return request_data back to blecon as response_data
     body = json.loads(event['body'])
-    events = []
-    for ev in body['request_data']['records']:
-        event = {
-            'device_id': body['network_headers']['device_id'],
-            'network_id': body['network_headers']['network_id'],
-            'account_id': body['network_headers']['account_id'],
-            'timestamp': ev['timestamp'],
-            'event_type': ev['event_type'],
-            'event_value': ev['event_value'],
-        }
-        events.append(event)
-
-    requests.post(os.environ.get('HANDLER'), json=events)
-    print("Posted {} to {}".format(events, os.environ.get('HANDLER')))
-    return dict(statusCode=200)
+    response = body.get('request_data', {})
+    return dict(statusCode=200, body={'response_data': response})
